e03d77731384237aa71d2753eb7746c3e25f405b
Edward Blanciak <blanciak.edward2@gmail.com>
2020-08-04 11:39:37 -0400

    'completed'

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -11,9 +11,43 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 
 # YOUR CODE HERE
 
+#THIS WAY YOU MUST CLOSE THE FILE
+
+fooFile = open('foo.txt', 'r')
+fileContent = fooFile.read()
+fooFile.close()
+
+print(fileContent)
+
+# THIS WAY DOESN'T REQUIRE YOU TO CLOSE THE FILE
+# PYTHON WILL CLOSE THE FILE FOR YOU
+# THIS WILL SET CONTENT TO NONE IN THE CASE OF THE FILE NO EXISTING
+
+try:
+    with open("foo.txt") as fooFile1:
+        content = fooFile1.read()
+except FileNotFoundError:
+    content = None
+
+print('\n')
+print(content)
+
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+names = ["Eddie", "John", "Tommy", "Pete"]
+
+with open('bar.txt', 'w') as barFile:
+    for name in names:                      # loops through names
+        barFile.write(name)                 # write each name to file
+        barFile.write("\n")                 # add new line after each name
+
+with open('bar.txt', 'r') as barFile:
+    barContent = barFile.read()
+
+print('\n')           # Separates with a new line.
+print(barContent)     # Prints our file contents.
+print(barFile.closed) # Shows if the file was closed or not.
