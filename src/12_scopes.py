db537ed4263f55efe0273188f7212d8449e2e723
Edward Blanciak <blanciak.edward2@gmail.com>
2020-08-04 13:15:43 -0400

    'completed'

--- src/12_scopes.py
+++ src/12_scopes.py
@@ -1,24 +1,30 @@
 # Experiment with scopes in Python.
 # Good reading: https://www.programiz.com/python-programming/global-local-nonlocal-variables
 
-# When you use a variable in a function, it's local in scope to the function.
+# !! When you use a variable in a function, it's local in scope to the function.  !!
 x = 12
 
 def change_x():
+    global x            # Allows us to access the global x variable
+    print(x)            # 12
     x = 99
 
-change_x()
+change_x()              # 99
 
-# This prints 12. What do we have to modify in change_x() to get it to print 99?
+# !! This prints 12. What do we have to modify in change_x() to get it to print 99? !!
+# Ans: You have to add the global keyword to the function with the variable you want to access.
 print(x)
 
 
 # This nested function has a similar problem.
+#### WHY DO I HAVE TO HAVE 2 GLOBAL Y'S ?
 
 def outer():
+    global y
     y = 120
 
     def inner():
+        global y
         y = 999
 
     inner()
@@ -30,3 +36,4 @@ def outer():
 
 
 outer()
+print(f"y after execution: {y}")
